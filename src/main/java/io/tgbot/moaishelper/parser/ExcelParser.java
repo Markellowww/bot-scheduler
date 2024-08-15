1e697f7be07e9cd433f4ec62be41d763dd3c6f41
KrivosheyYuriy <lonespy@yandex.ru>
2024-08-15 15:35:09 +0300

    Apache Poi library(versions should be the same!) + ExcelParser v0.1

---
+++ src/main/java/io/tgbot/moaishelper/parser/ExcelParser.java
@@ -0,0 +1,48 @@
+package io.tgbot.moaishelper.parser;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+
+public class ExcelParser {
+    public static void main(String[] args) throws IOException {
+        readExcel("src/main/java/io/tgbot/moaishelper/parser/Schedule.xlsx"); // тут нужен нормальный путь
+        // можно через String.format
+    }
+
+    public static void readExcel(String fileName) throws IOException {
+        try(XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(fileName))) {
+            XSSFSheet mySheet = myExcelBook.getSheetAt(0); // первая таблица
+            XSSFRow currentRow; // текущая строка из excel
+            short rowIndex = 3; // текущая строка по индексу (см шаблон)
+
+            for (short day = 1; day < 7; day++, rowIndex += 2) { // обход по дням
+                for (short pair = 1; pair < 8; pair++, rowIndex++) { // обход пар в дне
+                    currentRow = mySheet.getRow(rowIndex); // получаем текущий столбец
+                    short cellIndex = 1; // индекс клетки в строке
+                    for (short week = 0; week < 2; week++) { // обход по неделям (числитель -> знаменатель)
+                        String subject = currentRow.getCell(cellIndex++).toString();
+                        String teacher = currentRow.getCell(cellIndex++).toString();
+                        String auditorium  = currentRow.getCell(cellIndex++).toString();
+
+//                        System.out.println(subject + " " + teacher + " " + auditorium);
+                        if (!Stream.of(subject, teacher, auditorium).allMatch(String::isEmpty)) {
+                            /* добавляем в json данные
+                            [четность недели -> [номер дня -> [номер пары, предмет, преподаватель, аудитория]]]*/
+                        }
+                        cellIndex += 2; // смещаемся к полям знаменателя
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e) { // если не найден файл
+            System.out.println("File not found: " + fileName);
+        }
+    }
+}
