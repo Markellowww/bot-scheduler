f6c2148815d3aecc3cad93169ef9473f2e917849
Mark <148067937+Markelloww@users.noreply.github.com>
2024-08-29 17:24:56 +0300

    HotFix

--- src/main/java/io/tgbot/moaishelper/service/CommandInitializer.java
+++ src/main/java/io/tgbot/moaishelper/service/CommandInitializer.java
@@ -12,15 +12,5 @@ import java.util.List;
 public class CommandInitializer {
     public static void init(List<BotCommand> listOfCommands) {
         listOfCommands.add(new BotCommand("/start", "Начало работы"));
-        listOfCommands.add(new BotCommand("/creategroup", "Создать группу"));
-        listOfCommands.add(new BotCommand("/selectgroup", "Выбрать группу"));
-        listOfCommands.add(new BotCommand("/leavegroup", "Покинуть группу"));
-        listOfCommands.add(new BotCommand("/deletegroup", "Удалить группу"));
-        listOfCommands.add(new BotCommand("/invite", "Добавить пользователя в вашу группу"));
-        listOfCommands.add(new BotCommand("/kick", "Исключить пользователя из группы"));
-        listOfCommands.add(new BotCommand("/members", "Показать всех пользователей группы"));
-        listOfCommands.add(new BotCommand("/giveowner", "Передать роль владельца участнику группы"));
-        listOfCommands.add(new BotCommand("/setadmin", "Выдать роль админа участнику группы"));
-        listOfCommands.add(new BotCommand("/removeadmin", "Снять роль админа у участника группы"));
     }
 }
